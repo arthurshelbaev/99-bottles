ca45c42080916454a5c3ac1844f057fc31f3f3f0
arthurshelbaev <konodiobrandoda@mail.ru>
2022-11-19 10:17:37 +0300

    ||

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function song () {
         
         if (i >= 11 && i <= 19) bottles = "бутылок", bottles2 = "бутылок"
         else if (i % 10 === 5) bottles = "бутылок", bottles2 = "бутылки"
-        else if (i % 10 === 4 && i % 10 === 3) bottles = "бутылки", bottles2 = "бутылки"
+        else if (i % 10 === 4 || i % 10 === 3) bottles = "бутылки", bottles2 = "бутылки"
         else if (i % 10 === 2) bottles = "бутылки", bottles2 = "бутылка"
         else if (i === 1) return console.log((`${i} бутылка пива на стене, ${i} бутылка пива! Возьми одну, пусти по кругу. Нет бутылок пива на стене!`))
         else if (i % 10 === 1) bottles = "бутылка", bottles2 = "бутылок"
